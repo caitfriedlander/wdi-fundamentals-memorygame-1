5a96355fcb12cb954790abb4f66c03a931ab4603
Cait Friedlander <caitfinchart@gmail.com>
2018-05-29 16:48:56 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
-console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+  } else {
+  alert("Sorry, try again.");
+  }
+};
 
 
 var flipCard = function (cardId) {
-  // var cardOne = cards[0];
-  // cardsInPlay.push(cardOne);
-  // console.log("User flipped " + cardOne);
-  // var cardTwo = cards[2];
-  // cardsInPlay.push(cardTwo);
-  // console.log("User flipped " + cardTwo);
   cardsInPlay.push(cards[cardId])
   console.log("User flipped " + cards[cardId])
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
+      checkForMatch();
     } else {
-      alert("Sorry, try again.");
+      checkForMatch();
     }
-  };
-}
+  }
+};
 
 flipCard(0);
 flipCard(2);

c2c6773a9a2bd42097d2262ce8e63cea18417323
Cait Friedlander <caitfinchart@gmail.com>
2018-05-29 16:40:56 -0700

    drying code

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-  }
-};
+
+
+
+var flipCard = function (cardId) {
+  // var cardOne = cards[0];
+  // cardsInPlay.push(cardOne);
+  // console.log("User flipped " + cardOne);
+  // var cardTwo = cards[2];
+  // cardsInPlay.push(cardTwo);
+  // console.log("User flipped " + cardTwo);
+  cardsInPlay.push(cards[cardId])
+  console.log("User flipped " + cards[cardId])
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+  };
+}
+
+flipCard(0);
+flipCard(2);

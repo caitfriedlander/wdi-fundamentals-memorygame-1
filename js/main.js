867bc5ea55caca720a488a9c2934c0133e96d97b
Cait Friedlander <caitfinchart@gmail.com>
2018-05-29 20:41:00 -0700

    finished game logic

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
@@ -31,19 +32,28 @@ var checkForMatch = function () {
 };
 
 
-var flipCard = function (cardId) {
-  cardsInPlay.push(cards[cardId].rank);
+var flipCard = function () {
+  var cardId = this.getAttribute('data-id');
+
   console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank)
+
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
-  if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      checkForMatch();
-    } else {
-      checkForMatch();
-    }
+  this.setAttribute('src', cards[cardId].cardImage);
+   if (cardsInPlay.length === 2) {
+    checkForMatch();
   }
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+  document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+createBoard();
